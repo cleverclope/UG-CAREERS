88a1d157f2d07f6165a99efa2bf60a1e718e0f4e
Arinda Clever <48243275+cleverclope@users.noreply.github.com>
2022-04-02 22:33:57 +0300

    career major components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManagementComponent } from './management/management/management.component';
+import { AccountComponent } from './account/account/account.component';
+import { AuthenticationComponent } from './auth/authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManagementComponent,
+    AccountComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
